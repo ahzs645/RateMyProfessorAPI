0e6a72d6362367ffdc736139410e426c1da4a4a5
Sebi <167681990+sejager@users.noreply.github.com>
2024-10-14 20:43:56 -0400

    Update example.py
    modified for less failures

--- examples/example.py
+++ examples/example.py
@@ -1,13 +1,18 @@
-import ratemyprofessor
+import ratemyprofessor as rmp
 
-professor = ratemyprofessor.get_professor_by_school_and_name(
-    ratemyprofessor.get_school_by_name("Case Western Reserve University"), "Connamacher")
-if professor is not None:
-    print("%s works in the %s Department of %s." % (professor.name, professor.department, professor.school.name))
-    print("Rating: %s / 5" % professor.rating)
-    print("Difficulty: %s / 5" % professor.difficulty)
-    print("Total Ratings: %s" % professor.num_ratings)
-    if professor.would_take_again is not None:
-        print(("Would Take Again: %s" % round(professor.would_take_again, 1)) + '%')
+school = rmp.get_school_by_name("Lowellfdss")
+if school == None:
+    print("Nothing found, maybe check your spelling?")
+else:
+    professor = rmp.get_professor_by_school_and_name(school, "Cindy Chen")
+    if professor is not None:
+        print("%s works in the %s Department of %s." % (professor.name, professor.department, professor.school.name))
+        print("Rating: %s / 5.0" % professor.rating)
+        print("Difficulty: %s / 5.0" % professor.difficulty)
+        print("Total Ratings: %s" % professor.num_ratings)
+        if professor.would_take_again is not None:
+            print(("Would Take Again: %s" % round(professor.would_take_again, 1)) + '%')
+        else:
+            print("Would Take Again: N/A")
     else:
-        print("Would Take Again: N/A")
+        print("Nothing found, maybe check your spelling?")
