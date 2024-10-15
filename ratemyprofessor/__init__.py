4bde62b7a85f6008ec215d703ed4adcb15297293
Sebi <167681990+sejager@users.noreply.github.com>
2024-10-14 20:42:43 -0400

    Update __init__.py
    modified to pick closest match and not just top
    also a few more safeguards against blank searches

--- ratemyprofessor/__init__.py
+++ ratemyprofessor/__init__.py
@@ -4,14 +4,18 @@ ratemyprofessor
 RateMyProfessor API
 An extremely basic web scraper for the RateMyProfessor website.
 
+Original
 :copyright: (c) 2021 Nobelz
 :license: Apache 2.0, see LICENSE for more details.
+
+Updated in 2024 by sejager
 """
 import requests
 import re
 import json
 import base64
 import os
+import difflib
 
 from .professor import Professor
 from .school import School
@@ -20,9 +24,49 @@ from .school import School
 with open(os.path.join(os.path.dirname(__file__), "json/header.json"), 'r') as f:
     headers = json.load(f)
 
+with open(os.path.join(os.path.dirname(__file__), "json/schoolquery.json"), 'r') as f:
+    school_query = json.load(f)
+
 
 def get_school_by_name(school_name: str):
     """
+    NEW VERSION by sejager
+    Gets a School with the name closest to the search string
+    """
+    
+    school_name.replace(' ', '+')
+    url = "https://www.ratemyprofessors.com/search/schools?q=%s" % school_name
+    page = requests.get(url)
+    # Create an array of all the school names found via the search function
+    pat = r"(?<=(\"name\":\")).+?(?=\")"
+    matches = re.finditer(pat, page.text)
+    school_list = []
+    for match in matches:
+            school_list.append(match.group())
+    # Get the closest match and its index
+    closest_match = difflib.get_close_matches(school_name, school_list, 1, 0.1)
+    # If the string isn't close enough to anything try old method of just getting top of list
+    try:
+        closest_match_index = school_list.index(closest_match[0])
+    except IndexError:
+        schools = get_schools_by_name(school_name)
+        if schools:
+            return schools[0]
+        else:
+            return None
+
+    # Create an array of the school IDs and find the wanted one using the index above
+    data = re.findall(r'"legacyId":(\d+)', page.text)
+    school_list = []
+    for school_data in data:
+        try:
+            school_list.append(School(int(school_data)))
+        except ValueError:
+            pass
+    return school_list[closest_match_index]
+
+    """
+    OLD VERSION
     Gets a School with the specified name.
 
     This only returns 1 school name, so make sure that the name is specific.
@@ -31,13 +75,13 @@ def get_school_by_name(school_name: str):
 
     :param school_name: The school's name.
     :return: The school that match the school name. If no schools are found, this will return None.
-    """
+    
     schools = get_schools_by_name(school_name)
     if schools:
         return schools[0]
     else:
         return None
-
+    """
 
 def get_schools_by_name(school_name: str):
     """
@@ -49,18 +93,16 @@ def get_schools_by_name(school_name: str):
     :param school_name: The school's name.
     :return: List of schools that match the school name. If no schools are found, this will return an empty list.
     """
-    school_name.replace(' ', '+')
-    url = "https://www.ratemyprofessors.com/search/schools?q=%s" % school_name
+    no_space_school = school_name.replace(' ', '+')
+    url = "https://www.ratemyprofessors.com/search/schools?q=%s" % no_space_school
     page = requests.get(url)
     data = re.findall(r'"legacyId":(\d+)', page.text)
     school_list = []
-
     for school_data in data:
         try:
             school_list.append(School(int(school_data)))
         except ValueError:
             pass
-    
     return school_list
 
 
@@ -77,6 +119,7 @@ def get_professor_by_school_and_name(college: School, professor_name: str):
     :param professor_name: The professor's name.
     :return: The professor that matches the school and name. If no professors are found, this will return None.
     """
+    
     professors = get_professors_by_school_and_name(college, professor_name)
     max_professor = None
 
@@ -101,6 +144,7 @@ def get_professors_by_school_and_name(college: School, professor_name: str):
              this will return an empty list.
     """
     # professor_name.replace(' ', '+')
+    
     url = 'https://www.ratemyprofessors.com/search/professors/%s?q=%s' % (college.id, professor_name)
     page = requests.get(url)
     data = re.findall(r'"legacyId":(\d+)', page.text)
