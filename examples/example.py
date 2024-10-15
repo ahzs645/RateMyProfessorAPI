0330fe318224348e8b53a214f745c70eb23048ab
Sebi <167681990+sejager@users.noreply.github.com>
2024-10-14 20:53:19 -0400

    Update example.py

--- examples/example.py
+++ examples/example.py
@@ -1,5 +1,21 @@
 import ratemyprofessor as rmp
 
+professor = rmp.get_professor_by_school_and_name(rmp.get_school_by_name("University of Massachusetts Lowell"), "Daly")
+if professor is not None:
+    print("%s works in the %s Department of %s." % (professor.name, professor.department, professor.school.name))
+    print("Rating: %s / 5.0" % professor.rating)
+    print("Difficulty: %s / 5.0" % professor.difficulty)
+    print("Total Ratings: %s" % professor.num_ratings)
+    if professor.would_take_again is not None:
+        print(("Would Take Again: %s" % round(professor.would_take_again, 1)) + '%')
+    else:
+        print("Would Take Again: N/A")
+else:
+    print("Nothing found, maybe check your spelling?")
+
+# This one needs to be fixed
+"""import ratemyprofessor as rmp
+
 school = rmp.get_school_by_name("Lowellfdss")
 if school == None:
     print("Nothing found, maybe check your spelling?")
@@ -16,3 +32,4 @@ else:
             print("Would Take Again: N/A")
     else:
         print("Nothing found, maybe check your spelling?")
+"""
