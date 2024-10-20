06073e6187ae88ca01ad8f7b896269941abb03f6
Sebi <sebigyger@gmail.com>
2024-10-19 22:29:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ with open('requirements.txt') as f:
 
 setup(
     name='RateMyProfessorAPI',
-    version='1.3.6',
+    version='1.4.0',
     description='Python web scraper to get professor ratings from ratemyprofessor.com website.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/Nobelz/RateMyProfessorAPI',
-    author='Nobel Zhou',
-    author_email='nxz157@case.edu',
+    url='https://github.com/sejager/RateMyProfessorAPI',
+    author='Sebastian Gyger',
+    author_email='sebigyger@gmail.com',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -36,6 +36,6 @@ setup(
     include_package_data=True,
     install_requires=requirements,
     project_urls={
-        'Issue Tracker': 'https://github.com/Nobelz/RateMyProfessorAPI/issues',
+        'Issue Tracker': 'https://github.com/sejager/RateMyProfessorAPI/issues',
     }
 )
